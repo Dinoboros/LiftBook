df516c2cd8fd22bd10b7b90212dfbf06b040063f
Dinoboros <meryl.valier@gmail.com>
2025-09-07 12:28:57 +0200

    Initial Commit

---
+++ LiftBook/LiftBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftBookApp.swift
+//  LiftBook
+//
+//  Created by Méryl VALIER on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct LiftBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiftBookTests/LiftBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftBookTests.swift
+//  LiftBookTests
+//
+//  Created by Méryl VALIER on 07/09/2025.
+//
+
+import Testing
+@testable import LiftBook
+
+struct LiftBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
