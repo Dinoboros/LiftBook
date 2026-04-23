fc9334a051856d7dea077681f883a5948cb963ac
Dinoboros <meryl.valier@gmail.com>
2026-04-24 00:14:46 +0200

    Initial Commit

---
+++ LiftBook/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LiftBook
+//
+//  Created by Méryl VALIER on 24/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
